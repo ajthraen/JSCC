69523af85e5151bf2298b31108dff188fd8dfcad
AJ <ajthraen@gmail.com>
2022-08-14 20:57:29 -0400

    JSCC2

---
+++ app.js
@@ -0,0 +1,27 @@
+// console.log('Test'[0])
+// console.log('Test' + ' 3')
+// console.log('Test'[0] + ' 3'[1])
+// console.log('Test'.length)
+// let str = 'Andrew'
+// console.log(str)
+// console.log(str[0])
+// let str_length = str.length
+// console.log(str_length - 1)
+// let celsius = 10
+// let fdeg = (celsius * 1.8) + 32
+// console.log(fdeg)
+// let name = "Andrew"
+// let name2 = "Andy"
+// let bool = 'Andy' === name2
+// console.log(bool)
+let subscribed = false
+let loggedin = true
+if (subscribed === true) {
+    console.log('Show the Video')
+}
+else if (loggedin === true){
+    console.log('Enroll Today')
+}
+else{
+    console.log('Sign In')
+}
