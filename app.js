88ca2af4499d912fcd30d9f4b2a7c24ad7b8e9c7
AJ <ajthraen@gmail.com>
2022-08-14 21:58:46 -0400

    JSCC4

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//DATA TYPES
 // console.log('Test'[0])
 // console.log('Test' + ' 3')
 // console.log('Test'[0] + ' 3'[1])
@@ -14,6 +15,8 @@
 // let name2 = "Andy"
 // let bool = 'Andy' === name2
 // console.log(bool)
+
+//CONDITIONALS
 // let subscribed = false
 // let loggedin = true
 // if (subscribed === true) {
@@ -25,16 +28,52 @@
 // else{
 //     console.log('Sign In')
 // }
-let cash = 30
-let price = 40
-let balance = price - cash
+// let cash = 30
+// let price = 40
+// let balance = price - cash
 
-if (cash > price) {
-    console.log(`We are giving you $${(balance*-1)} dollars in change`)
-}
-else if (cash === price) {
-    console.log(`You paid, have a nice day!`)
-}
-else {
-    console.log(`You still owe $${balance} dollars`)
-}
+// if (cash > price) {
+//     console.log(`We are giving you $${(balance*-1)} dollars in change`)
+// }
+// else if (cash === price) {
+//     console.log(`You paid, have a nice day!`)
+// }
+// else {
+//     console.log(`You still owe $${balance} dollars`)
+// }
+// let hasCC = false
+// let hasCash = true
+
+// if (hasCC || hasCash) {
+//     console.log('they can pay')
+// }
+// else {
+//     console.log('they broke')
+// }
+
+// let hot = false
+// hot ? console.log("its hot") : console.log("its cold")
+
+//LOOPS
+// let count = 1
+// while (count <= 5) {
+//     console.log(count)
+//     count = count + 1
+// }
+
+// for (let i = 1; i <= 5; i++){
+//     console.log(i)
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//     if ((i % 3) == 0 && (i % 5) == 0) {console.log('Frontend Simplified')}
+//     else if ((i % 3) == 0) {console.log('Frontend')}
+//     else if ((i % 5) == 0) {console.log('Simplified')}
+//     else {console.log(i)}
+// }
+
+// let str = 'FrontEnd'
+// let charCount = str.length
+// for (let i = 0; i < charCount; i++) {
+//     console.log(str[i])
+// }
